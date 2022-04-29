59e83b381b52c6ac134d7153e826a67ff3c4d493
Komal Dhole <k.dhole@zensar.com>
2022-04-29 15:44:25 +0530

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+type TUser = {
+  name : string;
+  age : number;
+  bio : string;
+  gender : string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'products';
+export class AppComponent { 
+  name : string; 
+  age : number;
+  bio : string;
+  gender : string;
+  // name="";
+  // age ="";
+  // bio ="";
+  // gender ="";
+  user : TUser[] = []
+  
+  handleUser(event : MouseEvent)
+  {
+    event.preventDefault();
+    
+    console.log(this.name,this.age,this.bio,this.gender)
+    this.user.push(
+      {name : this.name,
+      age : this.age,
+      bio : this.bio,
+      gender : this.gender
+      }
+    )
+    console.log(this.user)
+    
+  
+  }
 }
